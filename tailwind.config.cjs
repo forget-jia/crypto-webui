012141faab0cc7036e8007d1a6811b5022e67648
forget-jia <58678526+forget-jia@users.noreply.github.com>
2025-10-16 16:03:16 +0800

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#7C3AED",
+      }
+    }
+  },
+  plugins: []
+};
